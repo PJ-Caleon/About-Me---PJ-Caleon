1d5d67ee3ce14637cfb7b375eead224c4bcc01d3
pj <pj@vbox.myguest.virtualbox.org>
2025-08-19 10:03:06 +0800

    m

--- assets/js/download.js
+++ assets/js/download.js
@@ -10,7 +10,7 @@ function setupDownloadButton() {
   button.addEventListener("click", () => {
     console.log("📄 Downloading resume PDF...");
     const link = document.createElement("a");
-    link.href = "./assets/images/Philip_Jeremiah_Caleon_Resume.pdf";
+    link.href = "assets/images/Philip_Jeremiah_Caleon_Resume.pdf";
     link.download = "Philip_Jeremiah_Caleon_Resume.pdf";
     document.body.appendChild(link);
     link.click();
